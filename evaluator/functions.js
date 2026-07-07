e0554ce3c7c8ce47cbcac14c44248f952d738ef9
rigelseven <marsh.oliver@hotmail.com>
2026-07-07 19:49:02 +1200

    square root of negative numbers should be undefined

--- evaluator/functions.js
+++ evaluator/functions.js
@@ -1,8 +1,10 @@
+import safeSquareRoot from "../math/root.js";
+
 export const FUNCTIONS = {
     sin: a => a.sin(),
     cos: a => a.cos(),
     tan: a => a.tan(),
-    sqrt: a => a.sqrt(),
+    sqrt: a => safeSquareRoot(a),
     ln: a => a.ln(),
     log: a => a.log10()
 }
