79c97fd5d8d216fc71e5faa135a329ef93c7454c
rigelseven <marsh.oliver@hotmail.com>
2026-08-31 21:18:38 +1200

    Fix variables not initialising on first load

--- evaluator/variables.js
+++ evaluator/variables.js
@@ -1,19 +1,18 @@
 import { statusBar } from "../statusbar.js";
-import { decimalReplacer, decimalReviver } from "../storage.js";
 
 class VariableManager {
     constructor(storage) {
         this.storage = {
-            "A": new Decimal(storage["A"] ? storage["A"] : 0),
-            "B": new Decimal(storage["B"] ? storage["B"] : 0),
-            "C": new Decimal(storage["C"] ? storage["C"] : 0),
-            "D": new Decimal(storage["D"] ? storage["D"] : 0),
-            "E": new Decimal(storage["E"] ? storage["E"] : 0),
-            "F": new Decimal(storage["F"] ? storage["F"] : 0),
-            "X": new Decimal(storage["X"] ? storage["X"] : 0),
-            "Y": new Decimal(storage["Y"] ? storage["Y"] : 0),
-            "M": new Decimal(storage["M"] ? storage["M"] : 0),
-            "Ans": new Decimal(storage["Ans"] ? storage["Ans"] : 0)
+            "A": new Decimal(storage?.["A"] ?? 0),
+            "B": new Decimal(storage?.["B"] ?? 0),
+            "C": new Decimal(storage?.["C"] ?? 0),
+            "D": new Decimal(storage?.["D"] ?? 0),
+            "E": new Decimal(storage?.["E"] ?? 0),
+            "F": new Decimal(storage?.["F"] ?? 0),
+            "X": new Decimal(storage?.["X"] ?? 0),
+            "Y": new Decimal(storage?.["Y"] ?? 0),
+            "M": new Decimal(storage?.["M"] ?? 0),
+            "Ans": new Decimal(storage?.["Ans"] ?? 0)
         };
     }
     
@@ -22,7 +21,7 @@ class VariableManager {
     }
 
     setVariable(variable, value) {
-        if (variable == "M") statusBar.toggle("memory", this.storage["M"] !== Decimal(0));
+        if (variable == "M") statusBar.toggle("memory", value != "0");
         this.storage[variable] = value;
 
         localStorage.setItem(
@@ -35,11 +34,38 @@ class VariableManager {
 }
 
 // Read variables
-let savedVariables;
+let savedVariables = [];
 try {
     savedVariables = JSON.parse(
             localStorage.getItem("variables"),
             decimalReviver);
 } catch {;}
 export const variableManager = new VariableManager(savedVariables);
-statusBar.toggle("memory", variableManager.storage["M"] !== Decimal(0));
+statusBar.toggle("memory", variableManager.storage["M"] != "0");
+
+
+function decimalReplacer(key, value) {
+    if (value instanceof Decimal) {
+        return {
+            s: Number(value.s),
+            e: Number(value.e),
+            d: value.d.map(Number)
+        };
+    }
+
+    return value;
+}
+
+function decimalReviver(key, value) {
+    if (
+        value &&
+        typeof value === "object" &&
+        "s" in value &&
+        "e" in value &&
+        Array.isArray(value.d)
+    ) {
+        return new Decimal(value);
+    }
+
+    return value;
+}
