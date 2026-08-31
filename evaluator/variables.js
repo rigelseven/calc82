2cabc419040d6ae7a4e75695a4f2f93d52b81826
rigelseven <marsh.oliver@hotmail.com>
2026-09-01 01:29:30 +1200

    Fix variables not initialising on first load again

--- evaluator/variables.js
+++ evaluator/variables.js
@@ -4,16 +4,16 @@ import { statusBar } from "../statusbar.js";
 class VariableManager {
     constructor(storage) {
         this.storage = {
-            "A": VariableManager.parseValue(storage["A"]),
-            "B": VariableManager.parseValue(storage["B"]),
-            "C": VariableManager.parseValue(storage["C"]),
-            "D": VariableManager.parseValue(storage["D"]),
-            "E": VariableManager.parseValue(storage["E"]),
-            "F": VariableManager.parseValue(storage["F"]),
-            "X": VariableManager.parseValue(storage["X"]),
-            "Y": VariableManager.parseValue(storage["Y"]),
-            "M": VariableManager.parseValue(storage["M"]),
-            "Ans": VariableManager.parseValue(storage["Ans"]),
+            "A": VariableManager.parseValue(storage?.["A"]),
+            "B": VariableManager.parseValue(storage?.["B"]),
+            "C": VariableManager.parseValue(storage?.["C"]),
+            "D": VariableManager.parseValue(storage?.["D"]),
+            "E": VariableManager.parseValue(storage?.["E"]),
+            "F": VariableManager.parseValue(storage?.["F"]),
+            "X": VariableManager.parseValue(storage?.["X"]),
+            "Y": VariableManager.parseValue(storage?.["Y"]),
+            "M": VariableManager.parseValue(storage?.["M"]),
+            "Ans": VariableManager.parseValue(storage?.["Ans"]),
         };
     }
     
@@ -33,13 +33,13 @@ class VariableManager {
         return this.storage[variable];
     }
 
+
     static parseValue(val) {
-    if (!val) return new Decimal(0);
-    if (typeof val === 'object' && 'numerator' in val && 'denominator' in val) {
+    if (val && typeof val === 'object' && 'numerator' in val && 'denominator' in val) {
         return new Fraction(Decimal(val.numerator), Decimal(val.denominator));
     }
-    return new Decimal(val);
-}
+    return new Decimal(val ?? 0);
+    }
 }
 
 // Read variables
