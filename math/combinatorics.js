846da65c870f2c6ca29006e2ca5f1695be8c2b8d
rigelseven <marsh.oliver@hotmail.com>
2026-09-01 00:47:03 +1200

    speed up combinatorics

--- math/combinatorics.js
+++ math/combinatorics.js
@@ -1,11 +1,42 @@
 import factorial from "./factorial.js";
 
 export function permutation(n, r) {
-    // nPr = n!/(n-r)!
-    return factorial(n).div(factorial(n.minus(r)));
+    n = new Decimal(n);
+    r = new Decimal(r);
+
+    if (r.isNegative() || r.gt(n)) {
+        return new Decimal(0);
+    }
+
+    let result = new Decimal(1);
+
+    for (let i = new Decimal(0); i.lt(r); i = i.plus(1)) {
+        result = result.times(n.minus(i));
+    }
+
+    return result;
 }
 
 export function combination(n, r) {
-    // nCr = n!/(r!*(n-r)!)
-    return factorial(n).div(factorial(r).times(factorial(n.minus(r))))
+    n = new Decimal(n);
+    r = new Decimal(r);
+
+    if (r.isNegative() || r.gt(n)) {
+        return new Decimal(0);
+    }
+
+    // C(n, r) = C(n, n-r)
+    if (r.gt(n.div(2))) {
+        r = n.minus(r);
+    }
+
+    let result = new Decimal(1);
+
+    for (let i = new Decimal(1); i.lte(r); i = i.plus(1)) {
+        result = result
+            .times(n.minus(r).plus(i))
+            .div(i);
+    }
+
+    return result;
 }
